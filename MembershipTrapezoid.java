4c9479ca89a8eb32c52c4252789eb83f07a89fd8
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2020-12-26 20:45:40 +0200

    Implemented Interface and added simple documentation

--- MembershipTrapezoid.java
+++ MembershipTrapezoid.java
@@ -1,4 +1,4 @@
-class MembershipTrapezoid {
+class MembershipTrapezoid implements MembershipFunction{
     private float a, b, c, d;
 
     public MembershipTrapezoid(float a, float b, float c, float d) {
@@ -7,7 +7,16 @@ class MembershipTrapezoid {
         this.c = c;
         this.d = d;
     }
-
+    /*
+     * Let 'value' be a crisp value
+     * Let M_U(value) be the membership of 'value' in fuzzy set 'U'
+     * M_U(value) for a Trapezoidal function is a domain defined function:
+     *
+     *      M_U(value) = { out1,   a <= value <= b
+     *                   { out2,   b <  value <= c
+     *                   { out3,   c <  value <= d
+     *                   { 0,      d <  value < a
+     */
     public float getMembership(float value) {
         Line line = null;
         if (value >= a && value <= b) {
