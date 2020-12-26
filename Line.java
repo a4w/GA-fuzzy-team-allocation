d9638487f9d0a93301b095439653c68772ca58f9
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2020-12-26 20:44:52 +0200

    Added simple documentation

--- Line.java
+++ Line.java
@@ -7,10 +7,20 @@ class Line {
         this.yIntercept = yIntercept;
     }
 
+    /*
+     * y = a * x + b
+     * a = slope
+     * b = yIntercept
+     */
     public float getY(float x) {
         return (this.slope * x) + this.yIntercept;
     }
 
+    /*
+     * y = a * x + b
+     * a = slope = (yb - ya) / (xb - xa)
+     * b = yIntercept = (ya - slope * xa)
+     */
     public static Line fromTwoPoints(CartesianPoint a, CartesianPoint b) {
         final float slope = (b.getY() - a.getY()) / (b.getX() - a.getX());
         final float yIntercept = a.getY() - (slope * a.getX());
